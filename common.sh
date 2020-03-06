6262d7854fe236bd671e8560d5c334ed7c87aaa0
lzw <lzw122333@gmail.com>
2020-03-07 05:28:45 +0800

    up

--- common.sh
+++ common.sh
@@ -8,3 +8,7 @@ cd /hk/proxy-env/ && \
     \cp -f ./conf.d/default_server.key /etc/nginx/default_server.key && \
     nginx -t && nginx -s reload
 
+tar -zcvf my-zhbao.tar.gz my-zhbao
+scp my-zhbao.tar.gz root@com-money-7.5.inhosts.net:/hk/
+tar -zxvf my-zhbao.tar.gz
+
